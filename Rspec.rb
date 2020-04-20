50cff410fabeb209b2d2307ffd47fda0f78531bc
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-04-20 11:43:18 +0000

    Complete exercise 2

--- Rspec.rb
+++ Rspec.rb
@@ -1,3 +1,5 @@
+# exercise 1
+
 class Rspec
   def expect_to_be_equal(one, two)
     if one == two
@@ -11,3 +13,18 @@ end
 test = Rspec.new
 p "By putting 1 and 1: #{test.expect_to_be_equal(1,1)}"
 p "By putting 1 and 2: #{test.expect_to_be_equal(1,2)}"
+
+# exercise 2
+
+class Expect
+  def to_be_equal(one, two)
+    if one == two
+      "Test passes! :)" 
+    else
+      "Test fails! D:"
+    end
+  end
+end
+
+p "By putting 1 and 1: #{Expect.new.to_be_equal(1,1)}"
+p "By putting 1 and 2: #{Expect.new.to_be_equal(1,2)}"
